d038779313efad1b37f3c91c3b6966d511cc7ab9
Seu Nome <tihaguinho92@gmail.com>
2026-01-05 22:58:19 -0300

    js melhorado; css modificado, mais clean

--- js/cep.js
+++ js/cep.js
@@ -1,25 +1,39 @@
 const btnCep = document.querySelector('.btn button');
 const resultados = document.querySelector('.resultados');
 
+const inputUf = document.getElementById('uf');
+const inputCidade = document.getElementById('cidade');
+const inputLogradouro = document.getElementById('logradouro');
+
 resultados.style.visibility = 'hidden';
 
 btnCep.addEventListener('click', async () => {
-   const uf = document.getElementById('uf').value.trim();
-   const cidade = document.getElementById('cidade').value.trim();
-   const logradouro = document.getElementById('logradouro').value.trim();
+   const uf = inputUf.value.trim();
+   const cidade = inputCidade.value.trim();
+   const logradouro = inputLogradouro.value.trim();
 
    if (!uf || !cidade || !logradouro) {
-      resultados.textContent = 'Preencha todos os campos!';
+
+
+      resultados.style.visibility = 'visible';
+      resultados.innerHTML = `<p class="campo-vazio">Preencha todos os campos!</p>`;
+      btnCep.disabled = true;
+
+      setTimeout(() => {
+         resultados.style.visibility = 'hidden';
+         resultados.textContent = '';
+         btnCep.disabled = false;
+      }, 3000);
+      
       return;
    }
+
+   btnCep.disabled = true;
 
    try {
       const res = await fetch(`https://viacep.com.br/ws/${uf}/${cidade}/${logradouro}/json/`);
 
       const data = await res.json();
-
-
-      console.log(data)
 
       resultados.innerHTML = '';
       
@@ -27,18 +41,35 @@ btnCep.addEventListener('click', async () => {
       const ul = document.createElement('ul');
 
       data.forEach(endereco => {
-         const li = document.createElement('li');
-         li.innerHTML = `
-               <b>Rua:</b> ${endereco.logradouro} <br>
-               <b>Bairro:</b> ${endereco.bairro} <br>
-               <b>Cidade:</b> ${endereco.localidade} - ${endereco.uf}<br>
-               <b>CEP:</b> ${endereco.cep}
-         `;
-         ul.appendChild(li);
+         const liRua = document.createElement('li');
+         liRua.innerHTML = ` <b>Rua:</b> ${endereco.logradouro}`;
+         const liBairro = document.createElement('li');
+         liBairro.innerHTML = ` <b>Bairro:</b> ${endereco.bairro}`;
+         const liCidade = document.createElement('li');
+         liCidade.innerHTML = ` <b>Cidade:</b> ${endereco.localidade} - ${endereco.uf}`;
+         const liCep = document.createElement('li');
+         liCep.innerHTML = ` <b>CEP:</b> ${endereco.cep}`;
+         ul.appendChild(liRua);
+         ul.appendChild(liBairro);
+         ul.appendChild(liCidade);
+         ul.appendChild(liCep);
       });
       resultados.style.visibility = 'visible';
       resultados.appendChild(ul);
+      inputUf.value = '';
+      inputCidade.value = '';
+      inputLogradouro.value = '';
+      inputUf.focus();
    } catch (error) {
-      resultados.innerHTML = '<h1>Erro na consulta</h1>'
+      resultados.innerHTML = '<h1 class="erro">Erro na consulta</h1>'
+
+      setTimeout(() => {
+         resultados.style.visibility = 'hidden';
+         resultados.textContent = '';
+         btnCep.disabled = false;
+      }, 3000);
+   } finally {
+      btnCep.disabled = false;
    };
+
 });
